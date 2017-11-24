81e339ea247f7532da0b02eece585ad3e9fdc620
Benjamin Negrevergne <bnegreve@gmail.com>
2017-11-24 18:59:45 +0100

    Intial import

---
+++ src/test/java/fr/dauphine/javaavance/phineloop/MainTest.java
@@ -0,0 +1,16 @@
+package fr.dauphine.javaavance.phineloop; 
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest extends TestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testMain() {
+        assertTrue( true );
+    }
+}
