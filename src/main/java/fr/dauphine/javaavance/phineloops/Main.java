7da6e209a71a055e221feb3089c9d5665d171154
Benjamin Negrevergne <bnegreve@gmail.com>
2019-11-14 17:11:48 +0100

    Minor updates in Main.java

--- src/main/java/fr/dauphine/javaavance/phineloops/Main.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/Main.java
@@ -10,12 +10,31 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class Main {
-    private static String inputFile= null;  
-    private static String outputFile= null;
+    private static String inputFile = null;  
+    private static String outputFile = null;
     private static Integer width = -1;
     private static Integer height = -1;
     private static Integer maxcc = -1; 
     
+
+    private void generate(int width, int height, String outputFile){
+	// generate grid and store it to outputFile...
+	//...            
+    }
+
+    private boolean solve(String inputFile, String outputFile){
+	// load grid from inputFile, solve it and store result to outputFile...
+	// ...
+
+	return false; 
+    }
+
+    private boolean check(String inputFile){
+	// load grid from inputFile and check if it is solved... 
+	// ...
+
+	return false; 
+    }
     
     public static void main(String[] args) {
         Options options = new Options();
@@ -29,6 +48,7 @@ public class Main {
         options.addOption("o", "output", true, "Store the generated or solved grid in <arg>. (Use only with --generate and --solve.)");
         options.addOption("t", "threads", true, "Maximum number of solver threads. (Use only with --solve.)");
         options.addOption("x", "nbcc", true, "Maximum number of connected components. (Use only with --generate.)");
+	options.addOption("G", "gui", true, "Run with the graphic user interface.");
         options.addOption("h", "help", false, "Display this help");
         
         try {
@@ -40,50 +60,46 @@ public class Main {
             System.exit(1);
         }       
                 
-    try{    
-        if( cmd.hasOption( "g" ) ) {
-            System.out.println("Running phineloops generator.");
-            String[] gridformat = cmd.getOptionValue( "g" ).split("x");
-            width = Integer.parseInt(gridformat[0]);
-            height = Integer.parseInt(gridformat[1]); 
-            if(! cmd.hasOption("o")) throw new ParseException("Missing mandatory --output argument.");
-            outputFile = cmd.getOptionValue( "o" );
+	try{    
+	    if( cmd.hasOption( "g" ) ) {
+		System.out.println("Running phineloops generator.");
+		String[] gridformat = cmd.getOptionValue( "g" ).split("x");
+		width = Integer.parseInt(gridformat[0]);
+		height = Integer.parseInt(gridformat[1]); 
+		if(! cmd.hasOption("o")) throw new ParseException("Missing mandatory --output argument.");
+		outputFile = cmd.getOptionValue( "o" );
 
-            // generate grid and store it to outputFile...
-            //...            
-        }
-        else if( cmd.hasOption( "s" ) ) {
-            System.out.println("Running phineloops solver.");
-            inputFile = cmd.getOptionValue( "s" );
-            if(! cmd.hasOption("o")) throw new ParseException("Missing mandatory --output argument.");      
-            outputFile = cmd.getOptionValue( "o" );
-            boolean solved = false; 
+		generate(width, height, outputFile); 
+	    }
+	    else if( cmd.hasOption( "s" ) ) {
+		System.out.println("Running phineloops solver.");
+		inputFile = cmd.getOptionValue( "s" );
+		if(! cmd.hasOption("o")) throw new ParseException("Missing mandatory --output argument.");      
+		outputFile = cmd.getOptionValue( "o" );
+
+		boolean solved = solve(intputFile, outputFile); 
+
+		System.out.println("SOLVED: " + solved);            
+	    }
         
-            // load grid from inputFile, solve it and store result to outputFile...
-            // ...
-            
-            System.out.println("SOLVED: " + solved);            
-        }
-        
-        else if( cmd.hasOption( "c" )) {
-            System.out.println("Running phineloops checker.");
-            inputFile = cmd.getOptionValue( "c" );
-            boolean solved = false; 
-            
-            // load grid from inputFile and check if it is solved... 
-            //...
-            System.out.println("SOLVED: " + solved);           
-        }
-        else {
-            throw new ParseException("You must specify at least one of the following options: -generate -check -solve ");           
-        }
-        } catch (ParseException e) {
-        // TODO Auto-generated catch block
+	    else if( cmd.hasOption( "c" )) {
+		System.out.println("Running phineloops checker.");
+		inputFile = cmd.getOptionValue( "c" );
+            		
+		boolean solved = check(inputFile); 
+
+		System.out.println("SOLVED: " + solved);           
+	    }
+	    else {
+		throw new ParseException("You must specify at least one of the following options: -generate -check -solve ");           
+	    }
+	} catch (ParseException e) {
             System.err.println("Error parsing commandline : " + e.getMessage());
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp( "phineloops", options );         
             System.exit(1); // exit with error      
-    }
+	}
+
         System.exit(0); // exit with success                            
     }
 }
