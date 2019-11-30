23c9ef3121a7a78135ae0b23cc2b07c86ad4b910
bahth18 <thierno.bah@dauphine.eu>
2019-11-30 17:33:35 +0100

    Modifying the CSP.java and Variable.java classes.

--- phineloops-alt/src/main/java/fr/dauphine/javaavance/phineloops/model/CSP.java
+++ phineloops-alt/src/main/java/fr/dauphine/javaavance/phineloops/model/CSP.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * In this constraint satisfaction algorithm (CSP), we need:
+   a) A set of variables v = {x1, x2, ..., Xn}
+       Each variable Xi has a domain Di of the possible values.
+   b) A finite set of constraints C1, ...., Cm on the variables.
+   c) Each node is defined by an assignment of values ​​{Xi = Vi, Xj = Vj, ....}
+ * */
 public class CSP {
 	
 	private List<Variable> variables;
@@ -29,4 +36,8 @@ public class CSP {
 		return domains;
 	}
 
+	
+	public List<Variable> getScope(){ // List of variables satisfying the constraints
+		return null;
+	}
 }
