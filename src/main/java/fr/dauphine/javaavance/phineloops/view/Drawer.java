741052119c44741b198c92e72c9764b7107b1fdb
bahth18 <thierno.bah@dauphine.eu>
2019-12-18 15:31:29 +0100

    Creation of the draw () function in each Shape.

--- src/main/java/fr/dauphine/javaavance/phineloops/view/Drawer.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/view/Drawer.java
@@ -1,5 +1,11 @@
 package fr.dauphine.javaavance.phineloops.view;
 
-public class Drawer {
+import java.awt.Graphics;
+
+import fr.dauphine.javaavance.phineloops.model.Shape;
+
+public abstract class Drawer {
+	
+	public abstract void draw(Graphics g);
 
 }
--- src/main/java/fr/dauphine/javaavance/phineloops/view/QShapeDrawer.java
+++ src/main/java/fr/dauphine/javaavance/phineloops/view/QShapeDrawer.java
@@ -1,5 +1,22 @@
 package fr.dauphine.javaavance.phineloops.view;
 
-public class QShapeDrawer {
+import java.awt.Graphics;
+
+import fr.dauphine.javaavance.phineloops.model.QShape;
+
+public class QShapeDrawer extends Drawer {
+	
+	private QShape qShapeToDraw;
+	
+	public QShapeDrawer(QShape qs) {
+		// TODO Auto-generated constructor stub
+		qShapeToDraw = qs;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
